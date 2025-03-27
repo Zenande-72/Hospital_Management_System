46cd909f23038af6c0ed071631e8e1bf4df58008
AshlynJanuary-230577644 <230577644@mycput.ac.za>
2025-03-27 23:51:05 +0200

    My Basecode

--- src/test/java/za/ac/cput/repository/DoctorRepositoryTest.java
+++ src/test/java/za/ac/cput/repository/DoctorRepositoryTest.java
@@ -21,12 +21,15 @@ import java.util.List;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class DoctorRepositoryTest {
  private static IDoctorRepository repository = DoctorRepository.getRepository();
- private Doctor doctor= DoctorFactory.createDoctor("231456","Mark","Daniels","Cardiologist","56457812",5, "Fully admitted","markDaniels@cput.ac.za","M","Occupied" );
+ private Doctor doctor= DoctorFactory.createDoctor("231456","Mark","Daniels","Cardiologist",56457812,5, "Fully admitted","markDaniels@cput.ac.za","M","Occupied" );
 
 
     @Test
     @Order(1)
     void create() {
+       Doctor created= repository.create(doctor);
+       assertNotNull(created);
+       System.out.println(created.toString());
 
 
     }
@@ -34,20 +37,32 @@ class DoctorRepositoryTest {
     @Test
     @Order(2)
     void read() {
+       Doctor read = repository.read(doctor.getDoctorID());
+       assertNotNull(read);
+       System.out.println(read.toString());
+
     }
 
     @Test
     @Order(3)
     void update() {
+       Doctor newDoctor = new Doctor.Builder().copy(doctor).setFirstName("Carlos").build();
+       Doctor updated = repository.update(newDoctor);
+       assertNotNull(updated);
+       assertNotNull(updated.toString());
     }
 
     @Test
+    @Disabled
     @Order(4)
     void delete() {
+       assertTrue(repository.delete(doctor.getDoctorID()));
+       System.out.println("Doctor has been deleted");
     }
 
     @Test
     @Order(5)
     void getAll() {
+       System.out.println(repository.getAll());
     }
 }
