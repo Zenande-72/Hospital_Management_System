fa4166504d6d6031d6472b8c1f8ed420f79bfe5c
Jamie-Lee Franse <230837484@mycput.ac.za>
2025-03-27 23:17:13 +0200

    My Code

---
+++ src/test/java/za/ac/cput/repository/AppointmentRepositoryTest.java
@@ -0,0 +1,34 @@
+package za.ac.cput.repository;
+
+import org.junit.jupiter.api.*;
+
+/*
+AppointmentRepository.java
+AppointmentRepository Class
+Author: Jamie-Lee Franse 230837484
+Date: 25 March 2025
+*/
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class AppointmentRepositoryTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
